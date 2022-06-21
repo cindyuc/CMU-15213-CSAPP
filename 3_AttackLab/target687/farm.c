ecf9c5f5d05be85b213b38195c20f7dc80bc21d4
cindyuc <ccchenxinyu@gmail.com>
2022-06-21 01:50:48 -0400

    update attacklab

---
+++ 3_AttackLab/target687/farm.c
@@ -0,0 +1,228 @@
+/*
+ * Note: To get instruction addresses, DO NOT compile this file yourself.
+ * Instead, you should disassemble the existing rtarget binary.
+ */
+
+/* This function marks the start of the farm */
+int start_farm()
+{
+    return 1;
+}
+
+unsigned addval_258(unsigned x)
+{
+    return x + 3351742792U;
+}
+
+unsigned addval_431(unsigned x)
+{
+    return x + 3281031256U;
+}
+
+unsigned addval_356(unsigned x)
+{
+    return x + 2425510066U;
+}
+
+unsigned getval_276()
+{
+    return 3284633928U;
+}
+
+unsigned addval_475(unsigned x)
+{
+    return x + 2425411736U;
+}
+
+unsigned getval_136()
+{
+    return 3284633928U;
+}
+
+void setval_127(unsigned *p)
+{
+    *p = 2462550344U;
+}
+
+unsigned addval_392(unsigned x)
+{
+    return x + 3281031256U;
+}
+
+/* This function marks the middle of the farm */
+int mid_farm()
+{
+    return 1;
+}
+
+/* Add two arguments */
+long add_xy(long x, long y)
+{
+    return x+y;
+}
+
+unsigned addval_100(unsigned x)
+{
+    return x + 3286272328U;
+}
+
+unsigned getval_365()
+{
+    return 3523794633U;
+}
+
+unsigned addval_358(unsigned x)
+{
+    return x + 3525364361U;
+}
+
+unsigned getval_129()
+{
+    return 3286272360U;
+}
+
+unsigned addval_119(unsigned x)
+{
+    return x + 2430634440U;
+}
+
+unsigned addval_470(unsigned x)
+{
+    return x + 3374372488U;
+}
+
+unsigned addval_364(unsigned x)
+{
+    return x + 3221801601U;
+}
+
+void setval_173(unsigned *p)
+{
+    *p = 4190358145U;
+}
+
+unsigned getval_490()
+{
+    return 3281043849U;
+}
+
+unsigned getval_430()
+{
+    return 3281174921U;
+}
+
+void setval_141(unsigned *p)
+{
+    *p = 3264272009U;
+}
+
+unsigned addval_179(unsigned x)
+{
+    return x + 3286280520U;
+}
+
+void setval_388(unsigned *p)
+{
+    *p = 3674789512U;
+}
+
+void setval_159(unsigned *p)
+{
+    *p = 3222847881U;
+}
+
+unsigned getval_118()
+{
+    return 2497087852U;
+}
+
+unsigned addval_351(unsigned x)
+{
+    return x + 3252717896U;
+}
+
+unsigned getval_299()
+{
+    return 3351349703U;
+}
+
+unsigned addval_284(unsigned x)
+{
+    return x + 3380134281U;
+}
+
+void setval_414(unsigned *p)
+{
+    *p = 3286272344U;
+}
+
+void setval_456(unsigned *p)
+{
+    *p = 3229931177U;
+}
+
+void setval_292(unsigned *p)
+{
+    *p = 3677935241U;
+}
+
+unsigned getval_108()
+{
+    return 3397914108U;
+}
+
+unsigned getval_232()
+{
+    return 3286272328U;
+}
+
+unsigned getval_211()
+{
+    return 3281046217U;
+}
+
+void setval_296(unsigned *p)
+{
+    *p = 3375939977U;
+}
+
+void setval_383(unsigned *p)
+{
+    *p = 3281046217U;
+}
+
+unsigned getval_450()
+{
+    return 2430634248U;
+}
+
+unsigned getval_235()
+{
+    return 3281043865U;
+}
+
+void setval_174(unsigned *p)
+{
+    *p = 3383020169U;
+}
+
+unsigned addval_251(unsigned x)
+{
+    return x + 3285617067U;
+}
+
+unsigned addval_461(unsigned x)
+{
+    return x + 3677932169U;
+}
+
+unsigned addval_406(unsigned x)
+{
+    return x + 3281305993U;
+}
+
+/* This function marks the end of the farm */
+int end_farm()
+{
+    return 1;
+}
